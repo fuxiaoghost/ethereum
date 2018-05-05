1cba663e4e14ece95d4be4674911a662399eb7f5
Kirn <wangsguang@gmail.com>
2018-05-05 18:47:15 +0800

    update

---
+++ key/key.js
@@ -0,0 +1,9 @@
+import { randomBytes } from 'crypto';
+import secp256k1 from 'secp256k1';
+import sha3 from 'keccakjs';
+// const { randomBytes } = require('crypto')
+// const secp256k1 = require('secp256k1')
+// const SHA3 = require('keccakjs')
+
+
+console.log('test');
