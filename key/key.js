46bd43c827794c58d97e15a58419f11991a18d86
Kirn <wangsguang@gmail.com>
2018-05-06 18:56:52 +0800

    update

--- key/key.js
+++ key/key.js
@@ -11,6 +11,46 @@ function generatePrivateKey() {
         privateKey =  randomBytes(32)
 
     }while(!secp256k1.privateKeyVerify(privateKey));
-    return privateKey.hexSlice();
+    return privateKey;
 }
-console.log(generatePrivateKey());
+
+/**
+ * secp256k1(ECDSA)通过私钥生成公钥
+ * @param {string} privateKey 私钥
+ * @param {boolean} compressed 是否为压缩格式
+ */
+function generatePublicKey(privateKey, compressed = false) {
+    let publicKey = secp256k1.publicKeyCreate(privateKey, compressed);
+    return publicKey;
+}
+
+/**
+ * 地址：公钥的sha3-256编码的后20字节，16进制编码的字符串
+ * @param {string} publicKey 公钥
+ */
+function generateAddress(publicKey) {
+    let h = new SHA3(256);
+    h.update(publicKey.slice(1)); //去掉前缀
+    return h.digest('hex').slice(-40);
+}
+
+/**
+ * 导入16进制编码的私钥 
+ * @param {string} hexString 16进制编码的私钥
+ */
+function loadPrivateKeyFromHexString(hexString) {
+    if (hexString.slice(0, 2) == '0x') {
+        hexString = hexString.slice(2);
+    }
+    if (hexString.length != 64) {
+        return null;
+    }
+    return new Buffer(hexString, 'hex')
+}
+
+let privateKey = loadPrivateKeyFromHexString("833e376d0894438c72a02e0e026f601894992f43bbabdccdfd92bea15ef718bb");
+let publicKey = generatePublicKey(privateKey);
+
+console.log(privateKey.hexSlice());
+console.log(publicKey.hexSlice());
+console.log(generateAddress(publicKey));
