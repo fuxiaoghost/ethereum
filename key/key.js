7efd61234446e6447704f09aca0e14ffa64a57dc
Kirn <wangsguang@gmail.com>
2018-05-10 00:37:25 +0800

    update

--- key/key.js
+++ key/key.js
@@ -27,7 +27,7 @@ function generatePublicKey(privateKey, compressed = false) {
 }
 
 /**
- * 地址：公钥的sha3-256编码的后20字节，16进制编码的字符串
+ * 地址：公钥的keccak256编码的后20字节，16进制编码的字符串
  * @param {string} publicKey 公钥
  */
 function generateAddress(publicKey) {
@@ -78,20 +78,34 @@ function bufferFrom(v) {
     return v
 }
 
+/**
+ * secp256k1数据签名
+ * @param {*} msg 需要签名的数据
+ * @param {Buffer} privateKeyBuffer 私钥Buffer
+ */
 function sign(msg, privateKeyBuffer) {
     if (!secp256k1.privateKeyVerify(privateKey)) {
         console.log("Invalid private key！");
         return null;
     }
+    // 生成需要签名数据的keccak256哈希
     let hash = createKeccakHash('keccak256').update(msg).digest();
     let sig = secp256k1.sign(hash, privateKeyBuffer);
     let ret = {};
+    // 分离签名得到r,s,v
     ret.r = sig.signature.slice(0, 32);
     ret.s = sig.signature.slice(32, 64);
     ret.v = sig.recovery;
     return ret;
 }
 
+/**
+ * 通过签名和原始数据恢复公钥
+ * @param {*} r signature[0-32)
+ * @param {*} s signature(32,64]
+ * @param {*} v recovery(0 or 1)
+ * @param {*} msg 原始数据
+ */
 function recovery(r, s, v, msg) {
     let signature = Buffer.concat([bufferFrom(r), bufferFrom(s)], 64)
     let recovery = v;
@@ -103,14 +117,23 @@ function recovery(r, s, v, msg) {
     return secp256k1.publicKeyConvert(senderPubKey, false);
 }
 
+/**
+ * 
+ * @param {*} msg 原始数据
+ * @param {*} r signature[0-32)
+ * @param {*} s signature(32,64]
+ * @param {*} pubKeyBuffer 公钥
+ */
 function verify(msg, r, s, pubKeyBuffer) {
     let signature = Buffer.concat([bufferFrom(r), bufferFrom(s)], 64)
     let hash = createKeccakHash('keccak256').update(msg).digest();
     return secp256k1.verify(hash, signature, pubKeyBuffer);
 }
 
-
-
+/**
+ * 拼接签名
+ * @param {Object} ret 
+ */
 function signBuffer(ret) {
     return Buffer.concat([
         bufferFrom(ret.r),
@@ -122,8 +145,11 @@ function signBuffer(ret) {
 
 let privateKey = loadPrivateKeyFromHexString("833e376d0894438c72a02e0e026f601894992f43bbabdccdfd92bea15ef718bb");
 let publicKey = generatePublicKey(privateKey);
+console.log("地址" + ":" + generateAddress(publicKey));
+
 let msg = "Hello World";
 let signRet = sign(msg, privateKey);
+
 
 console.log("私钥" + ':' + privateKey.hexSlice());
 console.log("公钥" + ":" + publicKey.hexSlice());
