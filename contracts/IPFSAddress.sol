62c129809027c8559f06892366fc6a4aa2f42571
shuguang.wang <shuguang.wang@baichanghui.com>
2018-07-07 13:25:41 +0800

    update

--- contracts/IPFSAddress.sol
+++ contracts/IPFSAddress.sol
@@ -4,10 +4,12 @@ contract IPFSAddress {
     mapping(address => bytes32) public bytesIpfs;
     mapping (address=>string) public stringIpfs;
     
+    // save as bytes32
     function saveBytes(bytes32 ipfs) public {
         bytesIpfs[msg.sender] = ipfs;
     }
     
+    // save as string 
     function saveString(string ipfs) public {
         stringIpfs[msg.sender] = ipfs;
     }
